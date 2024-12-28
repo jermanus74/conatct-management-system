2b68e37fedcf0f12c01b8a91b891e17cd2bc2b70
Jermanus <133773488+jermanus74@users.noreply.github.com>
2024-12-28 13:41:23 +0300

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit a338446e66d7cf0736a8930b04a7787b2a864adc, reversing
    changes made to 9e54705509a88a73ba7307525e2511c08e144f3e.

--- main.cpp
+++ main.cpp
@@ -4,30 +4,174 @@
 #include <vector>
 #include <sstream>
 using namespace std;
+
 struct ContactStruct{
 //    attributes for contact
-    int id;
-    string fname,lname,address,email;
-    string fullname=fname+" "+lname;
+
+    string fullname,address,email,id,phoneNo;
+
 //    function to display contact details
     void display() const{
-        cout<<"Contact ID:"<<id<<"Name: "<<fullname<<"\nEmail: "<<email<<"\nAddress: "<<address<<endl;
+        cout<<"Contact ID:"<<id<<"\nName: "<<fullname<<"\nEmail: "<<email<<"\nAddress: "<<address<<endl;
     }
 };
+
 // Function prototypes
 void createContact();
 void readContact();
 void updateContact();
 void deleteContact();
-ContactStruct loadContact();
-void saveContact(const vector<ContactStruct>& products);
+vector<ContactStruct> loadContact();
+void saveContact(const vector<ContactStruct>& contact);
 
-const string filename = "contacts.txt";
+//contact storage room
+vector<ContactStruct> contactList;
+
+const string contact_file = "contacts.txt";
 int main(){
-    ContactStruct contacts=loadContact();
-}
-ContactStruct loadContact(){
-    ContactStruct contacts;
+    int choice{};
+   contactList =loadContact();
+    // contactList.clear();
 
-    return contacts;
+   cout<<"-----Welcome to Contacts Management System-----"<<endl;
+   cout<<"Do you want to\n"
+         "1.Add Contact\n"
+         "2.Edit Contact\n"
+         "3.Delete Contact\n"
+         "4.View Contacts\n"
+         "5.Exit"<<endl;
+   cout<<"Enter choice: ";
+   cin>>choice;
+   cin.ignore();
+
+switch(choice){
+case 1:
+createContact();
+break;
+case 2:
+break;
+case 3:
+break;
+case 4:
+    readContact();
+break;
+case 5:
+break;
+default:
+cout<<"Invalid choice"<<endl;
 }
+return 0;
+}
+void createContact(){
+    ContactStruct contacts;
+    bool idExist,fnameExist,emailExist,pExist;
+    do{
+        fnameExist=false;
+        cout<<"Enter fullname: ";
+        getline(cin,contacts.fullname);
+//        check if contact name exits from contact list
+        for (const auto& cname:contactList) {
+            if (contacts.fullname==cname.fullname){
+                fnameExist=true;
+                cout<<"\nThe name you have entered already exist.Enter another name"<<endl;
+                break;
+            }
+        }
+
+
+
+
+    }while(fnameExist);
+    do {
+        idExist=false;
+        cout<<"Enter contact ID: ";
+        getline(cin,contacts.id);
+        for (const auto& Cid: contactList) {
+            if (contacts.id==Cid.id){
+                idExist=true;
+                cout<<"Contact ID already exists.Enter another ID to continue"<<endl;
+                break;
+            }
+        }
+    } while (idExist);
+    do {
+        emailExist=false;
+        cout<<"Enter email: ";
+        getline(cin,contacts.email);
+        for (const auto&emailId:contactList) {
+            if (contacts.email==emailId.email){
+                emailExist=true;
+                cout<<"Email already exists.Enter another email to continue"<<endl;
+                break;
+            }
+        }
+    } while (emailExist);
+    do {
+        pExist=false;
+        cout<<"Enter phone number: ";
+        getline(cin,contacts.phoneNo);
+        for (const auto&pId:contactList) {
+            if (contacts.phoneNo==pId.phoneNo){
+                pExist=true;
+                cout<<"Phone number already exists.Enter another number to continue"<<endl;
+                break;
+            }
+        }
+    } while (pExist);
+    cout<<"Enter your Address: ";
+    getline(cin,contacts.address);
+    cin.ignore();
+   contactList.push_back(contacts);
+    saveContact(contactList);
+    cout << "Account created successfully!" << endl;
+}
+vector<ContactStruct> loadContact(){
+    vector<ContactStruct> contact;
+    if (ifstream file(contact_file); file.is_open()) {
+        ContactStruct contacts;
+        while (getline(file, contacts.id)) {
+            getline(file, contacts.fullname);
+            getline(file, contacts.email);
+            getline(file, contacts.phoneNo);
+            getline(file, contacts.address);
+            contact.push_back(contacts);
+        }
+        file.close();
+    } else {
+        cout << "No existing user database found. Starting fresh." << endl;
+    }
+    // contact.clear();
+return contact;
+
+}
+void saveContact(const vector<ContactStruct>& contact){
+    if (ofstream file(contact_file,ios::trunc); file.is_open()) {
+         for (const auto& c:contact) {
+             file<<"\nContact ID: "<<c.id
+                     <<"\nName: "<<c.fullname
+                     <<"\nEmail: "<<c.email
+                     <<"\nPhone: "<<c.phoneNo
+                     <<"\nAddress: "<<c.address
+                     <<endl;
+         }
+         file.close();
+     }
+    else {
+         cout << "No existing user database found. Starting fresh." << endl;
+     }
+}
+
+void readContact() {
+    if (contactList.empty()) {
+                           cout<<"No contact data found. Starting fresh." << endl;
+    } else {
+      cout<<"-------Contact data reading------"<<endl;
+        for (const auto& c:contactList) {
+            c.display();
+            cout<<"---------------------------------"<<endl;
+        }
+    }
+}
+
+
+
